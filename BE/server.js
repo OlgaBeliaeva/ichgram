e81dcedad51b1618752eb951f9bd98726c912a56
OlgaBeliaeva <olgabel505@web.de>
2025-01-19 01:15:14 +0100

    The main server is created in the server.js file, which runs the Express app and the WebSocket server.

---
+++ BE/server.js
@@ -0,0 +1,51 @@
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from 'socket.io';
+
+import connectDB from './config/db.js';
+import { messageSocketHandler, authenticateSocket } from './routes/messageRoutes.js';
+import { notificationnSocketHandler } from "./middlewares/notificationSocketHandler.js";
+import app from './app.js';
+
+dotenv.config();
+connectDB();
+
+ const server = http.createServer(app)
+
+ const PORT = process.env.PORT || 5001;
+ 
+ app.use(bodyParser.json({limit: '10mb'}));
+ app.use(bodyParser.urlencoded({limit: '10mb', extend: true}));
+
+ //Инициализируем WebSocket сервер
+ const io = new Server(server, {
+    cors: {
+        origin: "*",
+        metods: ["GET", "POST"],
+        credentials: true,
+        allowedHeaders: ["Authorization", "Content-Type"],
+    },
+    transports: ["websocket", "polling"],
+     });
+     // Сохраняем io в app для доступа из контролеров
+     app.set("io", io);
+     // Middleware для WebSocket - аутентификации
+     io.use((socket, next) => {
+        authenticateSocket(socket, next);  // проверяем JWT токен
+     });
+
+     // обрабатываем WebSocket - соединения
+     io.on('connection', (socket) =>{
+        console.log(' Новое WebSocket - соединение');
+
+      // подключаем обработчики сообщений
+      messageSocketHandler(socket, io);
+      
+      // подключаем обработчики уведомлений
+      notificationnSocketHandler(socket, io);
+     });
+
+     server.listen(PORT, () => {
+        console.log(`Сервер запущен на порту ${PORT}`);
+     });
